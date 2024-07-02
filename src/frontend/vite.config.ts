35ebc5a608f0769e7f7eee87bd537352559abedd
antoine lebaud <lebaud.antoine131@gmail.com>
2024-07-02 17:07:17 +0200

    🔧(frontend) pass host and port through environment vars
    Configured the frontend to use environment variables (prefixed with "VITE_") for frontend
    port and host configuration, which will be overridden in the Helm chart values
    to ensure correct values are used in different environments.
    
    Helm requires the frontend port to be 8081 and use the public host,
    not the default "localhost" value.

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+      host: env.VITE_HOST || 'localhost',
+    },
+  }
 })
