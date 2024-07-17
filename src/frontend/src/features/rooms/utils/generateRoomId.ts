d8c8ac0811da2a77759e77aac3692d43e89f7a29
lebaudantoine <lebaud.antoine131@gmail.com>
2024-07-17 21:56:21 +0200

    🚸(frontend) generate shorter room IDs making URLs easier to share
    UUID-v4 room IDs are long and uninviting. Shorter, custom room IDs
    can enhance UX by making URLs easier to share and remember.
    
    While UUID-v4s are typically used in database systems for their low
    collision probability, for ephemeral room IDs, the collision risk of e+14
    combinations is acceptable.
    
    This aligns room IDs with Google Meet format.
    
    Even if the 'slugify' function is not used anymore, I kept it.
    Lmk if you prefer removing it @manuhabitela

--- src/frontend/src/features/rooms/utils/generateRoomId.ts
+++ src/frontend/src/features/rooms/utils/generateRoomId.ts
@@ -1,5 +1,27 @@
-import { slugify } from '@/utils/slugify'
+
+const getRandomChar = () => {
+  // Google Meet uses only letters in a room identifier
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
+  return characters.charAt(Math.floor(Math.random() * charactersLength))
+}
+
+const generateSegment = (length: number): string => {
+  let segment = '';
+  for (let i = 0; i < length; i++) {
+    segment += getRandomChar();
+  }
+  return segment;
+};
 
 export const generateRoomId = () => {
-  return slugify(crypto.randomUUID())
+  // Generates a unique room identifier following the Google Meet format
+  const shortLength = 3;
+  const longLength = 4;
+  const parts = [
+    generateSegment(shortLength),
+    generateSegment(longLength),
+    generateSegment(shortLength)
+  ];
+  return parts.join('-');
 }
