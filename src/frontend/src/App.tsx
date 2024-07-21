e04b8d081fa106074967c4ab2ded1bac642c4f84
Emmanuel Pelletier <manu@habite.la>
2024-07-21 17:37:49 +0200

    🐛(frontend) have a suspense fallback
    the app crashed on screen changes, I don't quite get why right now… but
    at least this goes around the issue…

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import '@livekit/components-styles'
 import '@/styles/index.css'
+import { Suspense } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
 import { useLang } from 'hoofd'
 import { Route, Switch } from 'wouter'
+import { Screen } from './layout/Screen'
 import { HomeRoute } from '@/features/home'
-import { NotFound } from './routes/NotFound'
 import { RoomRoute, roomIdRegex } from '@/features/rooms'
+import { NotFound } from './routes/NotFound'
 import './i18n/init'
 
 const queryClient = new QueryClient()
@@ -17,12 +19,14 @@ function App() {
   useLang(i18n.language)
   return (
     <QueryClientProvider client={queryClient}>
-      <Switch>
-        <Route path="/" component={HomeRoute} />
-        <Route path={roomIdRegex} component={RoomRoute} />
-        <Route component={NotFound} />
-      </Switch>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <Suspense fallback={<Screen />}>
+        <Switch>
+          <Route path="/" component={HomeRoute} />
+          <Route path={roomIdRegex} component={RoomRoute} />
+          <Route component={NotFound} />
+        </Switch>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Suspense>
     </QueryClientProvider>
   )
 }
