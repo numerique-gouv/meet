70402ed756467c6bfd9fc908fd7561d7124d4e28
lebaudantoine <lebaud.antoine131@gmail.com>
2024-07-24 23:28:30 +0200

    ✨(frontend) try silent login every one hour
    This commit is totally work in progress.
    @manuhabitela the floor is yours.
    
    If the user is logged-out, try silently log-in her every hour,
    to avoid any manual login on her side.
    
    The one hour value has been discussed with @manuhabitela, but there
    is no real logic behind it. Could definitely be shorter or longer.
    
    It needs at least to be longer than the average silent login flow
    to avoid locking the user-agent in an infinite redirection loop.

---
+++ src/frontend/src/features/auth/utils/silentLogin.ts
@@ -0,0 +1,30 @@
+import { authUrl } from "@/features/auth";
+
+const SILENT_LOGIN_RETRY_KEY = 'silent-login-retry'
+
+const isRetryAllowed = () => {
+  const lastRetryDate = localStorage.getItem(SILENT_LOGIN_RETRY_KEY);
+  if (!lastRetryDate) {
+    return true;
+  }
+  const now = new Date();
+  return now.getTime() > Number(lastRetryDate)
+}
+
+const setNextRetryTime = (retryIntervalInSeconds: number) => {
+  const now = new Date()
+  const nextRetryTime = now.getTime() + (retryIntervalInSeconds * 1000);
+  localStorage.setItem(SILENT_LOGIN_RETRY_KEY, String(nextRetryTime));
+}
+
+const initiateSilentLogin = () => {
+  window.location.href = authUrl(true)
+}
+
+export const attemptSilentLogin = (retryIntervalInSeconds: number) => {
+  if (!isRetryAllowed()) {
+    return
+  }
+  setNextRetryTime(retryIntervalInSeconds)
+  initiateSilentLogin()
+}
