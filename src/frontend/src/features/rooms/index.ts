ee1abbed04929926adedbab51770eb2d4e0a7ab6
Emmanuel Pelletier <manu@habite.la>
2024-07-29 08:42:06 +0200

    🚸(feedback) remember the user about feedback form on call end
    add a new route that just tells the user about the feedback form

--- src/frontend/src/features/rooms/index.ts
+++ src/frontend/src/features/rooms/index.ts
@@ -1,3 +1,4 @@
 export { Room as RoomRoute } from './routes/Room'
+export { FeedbackRoute } from './routes/Feedback'
 export { roomIdPattern, isRoomValid } from './utils/isRoomValid'
 export { generateRoomId } from './utils/generateRoomId'
