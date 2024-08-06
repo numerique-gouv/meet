04c67bb70bf55b1944fc1842d9450131441f91bb
lebaudantoine <lebaud.antoine131@gmail.com>
2024-08-06 17:49:02 +0200

    ♻️(frontend) restore User Information in Settings Dialog
    Reintroduced user information in the settings dialog. This change allows
    users to see the account they are logged into, which is important for
    account management. An additional feature to update usernames will be
    added soon. Apologies to @manuhabitela for the oversight.

--- src/frontend/src/features/settings/components/SettingsDialog.tsx
+++ src/frontend/src/features/settings/components/SettingsDialog.tsx
@@ -1,14 +1,38 @@
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { useLanguageLabels } from '@/i18n/useLanguageLabels'
-import { Dialog, type DialogProps, Field, H } from '@/primitives'
+import { A, Badge, Dialog, type DialogProps, Field, H, P } from '@/primitives'
+import { authUrl, logoutUrl, useUser } from '@/features/auth'
 
 export type SettingsDialogProps = Pick<DialogProps, 'isOpen' | 'onOpenChange'>
 
 export const SettingsDialog = (props: SettingsDialogProps) => {
   const { t, i18n } = useTranslation('settings')
+  const { user, isLoggedIn } = useUser()
   const { languagesList, currentLanguage } = useLanguageLabels()
   return (
     <Dialog title={t('dialog.heading')} {...props}>
+      <H lvl={2}>{t('account.heading')}</H>
+      {isLoggedIn ? (
+        <>
+          <P>
+            <Trans
+              i18nKey="settings:account.currentlyLoggedAs"
+              values={{ user: user?.email }}
+              components={[<Badge />]}
+            />
+          </P>
+          <P>
+            <A href={logoutUrl()}>{t('logout', { ns: 'global' })}</A>
+          </P>
+        </>
+      ) : (
+        <>
+          <P>{t('account.youAreNotLoggedIn')}</P>
+          <P>
+            <A href={authUrl()}>{t('login', { ns: 'global' })}</A>
+          </P>
+        </>
+      )}
       <H lvl={2}>{t('language.heading')}</H>
       <Field
         type="select"
