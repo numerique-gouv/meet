bc08893b5f1aa378759e4064e74b90d8cef4b3b0
lebaudantoine <lebaud.antoine131@gmail.com>
2024-08-06 11:06:16 +0200

    🔧(frontend) configure Prettier and ESLint
    Set up Prettier with minimal configuration. Installed eslint-config-prettier to
    prevent conflicts between ESLint and Prettier.

--- src/frontend/.eslintrc.cjs
+++ src/frontend/.eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
     'plugin:jsx-a11y/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'styled-system'],
   parser: '@typescript-eslint/parser',
