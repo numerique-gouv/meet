d34f043dc8d5a9832cc5f7b57e752f73a111a071
lebaudantoine <lebaud.antoine131@gmail.com>
2024-08-06 17:25:22 +0200

    ♻️(frontend) allow opening Settings dialog in a controlled way
    I am not hundred percent sure it respects @manuhabitela's guidelines,
    but for the options menu, I would need to trigger a dialog in a controlled way.
    
    I tried to have the minimal impact on the existing settings dialog.

--- src/frontend/src/features/settings/components/SettingsDialog.tsx
+++ src/frontend/src/features/settings/components/SettingsDialog.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from 'react-i18next'
 import { useLanguageLabels } from '@/i18n/useLanguageLabels'
-import { Dialog, Field, H } from '@/primitives'
+import { Dialog, type DialogProps, Field, H } from '@/primitives'
 
-export const SettingsDialog = () => {
+export type SettingsDialogProps = Pick<DialogProps, 'isOpen' | 'onOpenChange'>
+
+export const SettingsDialog = (props: SettingsDialogProps) => {
   const { t, i18n } = useTranslation('settings')
   const { languagesList, currentLanguage } = useLanguageLabels()
   return (
-    <Dialog title={t('dialog.heading')}>
+    <Dialog title={t('dialog.heading')} {...props}>
       <H lvl={2}>{t('language.heading')}</H>
       <Field
         type="select"
