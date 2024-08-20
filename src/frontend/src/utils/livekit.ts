74b296aa379ffc104858fc1d4d8c26014a1d443c
lebaudantoine <lebaud.antoine131@gmail.com>
2024-08-20 10:11:21 +0200

    ✨(frontend) add utils to determine user's browser
    Copied from LiveKit utils functions.
    Using LiveKit client, determine whether the user's browser is
    Safari, chromium based, or firefox.

--- src/frontend/src/utils/livekit.ts
+++ src/frontend/src/utils/livekit.ts
@@ -1,5 +1,17 @@
-import { LogLevel, setLogLevel } from 'livekit-client'
+import { getBrowser, LogLevel, setLogLevel } from 'livekit-client'
 
 export const silenceLiveKitLogs = (shouldSilenceLogs: boolean) => {
   setLogLevel(shouldSilenceLogs ? LogLevel.silent : LogLevel.debug)
+}
+
+export function isFireFox(): boolean {
+  return getBrowser()?.name === 'Firefox'
+}
+
+export function isChromiumBased(): boolean {
+  return getBrowser()?.name === 'Chrome'
+}
+
+export function isSafari(): boolean {
+  return getBrowser()?.name === 'Safari'
 }
