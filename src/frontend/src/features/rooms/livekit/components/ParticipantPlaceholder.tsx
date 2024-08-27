7bbc33ab34b0a6f7159bd4ee9748c092ca9e0ea1
lebaudantoine <lebaud.antoine131@gmail.com>
2024-08-27 18:26:13 +0200

    💩(frontend) enhance participant's placeholder responsiveness
    Duplicated source from LiveKit internal hooks (not ideal, but necessary)
    to ensure the inner content of the participant placeholder consistently
    fits its parent container. Unfortunately, I couldn't find
    a simpler solution.
    
    This might lead to some performances issues, as for each tile, some js
    would be computing avatar's size, which is much more costly than pure
    css…
    
    Note: Gmeet achieves this by generating a temporary placeholder image
    with a colored  background and initials, allowing for a perfectly round,
    responsive avatar without relying on JavaScript.

--- src/frontend/src/features/rooms/livekit/components/ParticipantPlaceholder.tsx
+++ src/frontend/src/features/rooms/livekit/components/ParticipantPlaceholder.tsx
@@ -3,6 +3,8 @@ import { styled } from '@/styled-system/jsx'
 import { Avatar } from '@/components/Avatar'
 import { useIsSpeaking } from '@livekit/components-react'
 import { getParticipantColor } from '@/features/rooms/utils/getParticipantColor'
+import { useSize } from '@/features/rooms/livekit/hooks/useResizeObserver'
+import { useMemo, useRef } from 'react'
 
 const StyledParticipantPlaceHolder = styled('div', {
   base: {
@@ -24,17 +26,29 @@ export const ParticipantPlaceholder = ({
 }: ParticipantPlaceholderProps) => {
   const isSpeaking = useIsSpeaking(participant)
   const participantColor = getParticipantColor(participant)
+
+  const placeholderEl = useRef<HTMLDivElement>(null)
+  const { width, height } = useSize(placeholderEl)
+
+  const minDimension = Math.min(width, height)
+  const avatarSize = useMemo(
+    () => Math.min(Math.round(minDimension * 0.9), 160),
+    [minDimension]
+  )
+
+  const initialSize = useMemo(() => Math.round(avatarSize * 0.3), [avatarSize])
+
   return (
-    <StyledParticipantPlaceHolder>
+    <StyledParticipantPlaceHolder ref={placeholderEl}>
       <div
         style={{
           borderRadius: '50%',
           animation: isSpeaking ? 'pulse 1s infinite' : undefined,
-          width: '80%',
-          maxWidth: '150px',
           height: 'auto',
           aspectRatio: '1/1',
-          fontSize: '50px',
+          width: '80%',
+          maxWidth: `${avatarSize}px`,
+          fontSize: `${initialSize}px`,
         }}
       >
         <Avatar
