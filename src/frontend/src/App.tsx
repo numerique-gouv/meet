dea35448d99b751d2a6dd092bd48ab7a1b42cf21
lebaudantoine <lebaud.antoine131@gmail.com>
2024-09-23 18:52:45 +0200

    ♻️(frontend) encapsulate PostHog logic in dedicated functions
    Decouple the code from the PostHog dependency by wrapping it in a custom hook
    and several utility functions. This enhances code maintainability and separation
    of concerns.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import '@livekit/components-styles'
 import '@/styles/index.css'
-import { Suspense, useEffect } from 'react'
+import { Suspense } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
 import { useLang } from 'hoofd'
-import { Switch, Route, useLocation } from 'wouter'
+import { Switch, Route } from 'wouter'
 import { I18nProvider } from 'react-aria-components'
 import { Layout } from './layout/Layout'
 import { NotFoundScreen } from './components/NotFoundScreen'
@@ -13,28 +13,16 @@ import { routes } from './routes'
 import './i18n/init'
 import { silenceLiveKitLogs } from '@/utils/livekit.ts'
 import { queryClient } from '@/api/queryClient'
-import posthog from 'posthog-js'
+import { useAnalytics } from '@/features/analytics/hooks/useAnalytics'
 
 function App() {
   const { i18n } = useTranslation()
-  const [location] = useLocation()
   useLang(i18n.language)
 
   const isProduction = import.meta.env.PROD
   silenceLiveKitLogs(isProduction)
 
-  // We're on a free tier, so we need to limit the number of events we send to PostHog.
-  // Be frugal with event tracking, even though we could filter them out later if necessary.
-  if (isProduction) {
-    posthog.init('phc_RPYko028Oqtj0c9exLIWwrlrjLxSdxT0ntW0Lam4iom', {
-      api_host: 'https://eu.i.posthog.com',
-      person_profiles: 'always',
-    })
-  }
-
-  useEffect(() => {
-    posthog.capture('$pageview')
-  }, [location])
+  useAnalytics()
 
   return (
     <QueryClientProvider client={queryClient}>
