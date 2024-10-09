fe8fd3646767a6643c1ad1dc5fff34c776406f66
lebaudantoine <lebaud.antoine131@gmail.com>
2024-10-09 17:30:51 +0200

    ✨(frontend) introduce a TextArea primitive
    Needed for the Chat text message input.
    Basic styled RAC text area.

---
+++ src/frontend/src/primitives/TextArea.tsx
@@ -0,0 +1,18 @@
+import { TextArea as RACTextArea } from 'react-aria-components'
+import { styled } from '@/styled-system/jsx'
+
+/**
+ * Styled RAC TextArea.
+ */
+export const TextArea = styled(RACTextArea, {
+  base: {
+    width: 'full',
+    paddingY: 0.25,
+    paddingX: 0.5,
+    border: '1px solid',
+    borderColor: 'control.border',
+    color: 'control.text',
+    borderRadius: 4,
+    transition: 'all 200ms',
+  },
+})
