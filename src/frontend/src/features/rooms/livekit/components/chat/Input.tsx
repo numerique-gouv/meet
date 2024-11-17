8e62f2ecd3aa376731b2a91b9cf75903ff35d898
lebaudantoine <lebaud.antoine131@gmail.com>
2024-11-18 00:01:10 +0100

    🩹(frontend) fix TextArea export
    Wrongly exported the TextArea primitive, mybad.
    Fixed !

--- src/frontend/src/features/rooms/livekit/components/chat/Input.tsx
+++ src/frontend/src/features/rooms/livekit/components/chat/Input.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@/primitives'
+import { Button, TextArea } from '@/primitives'
 import { HStack } from '@/styled-system/jsx'
 import { RiSendPlane2Fill } from '@remixicon/react'
 import { useState, useEffect } from 'react'
-import { TextArea } from '@/primitives/TextArea'
 import { RefObject } from 'react'
 import { useTranslation } from 'react-i18next'
 
