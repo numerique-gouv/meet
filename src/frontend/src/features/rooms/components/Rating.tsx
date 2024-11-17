fb8c0fd1b516bba91791efc0b4521790a7cdf76c
lebaudantoine <lebaud.antoine131@gmail.com>
2024-11-17 22:48:40 +0100

    💄(frontend) refactor feedbacks screen
    Enhance initial screen. Allow user returning to the meeting.
    Also, add basic and non-functional rating component to mock,
    what the form would be.

---
+++ src/frontend/src/features/rooms/components/Rating.tsx
@@ -0,0 +1,114 @@
+import { Button, H, Text } from '@/primitives'
+import { useState } from 'react'
+import { cva } from '@/styled-system/css'
+import { useTranslation } from 'react-i18next'
+import { styled } from '@/styled-system/jsx'
+
+const Card = styled('div', {
+  base: {
+    border: '1px solid',
+    borderColor: 'gray.300',
+    padding: '1rem',
+    marginTop: '1.5rem',
+    borderRadius: '0.25rem',
+    boxShadow: '',
+  },
+})
+
+const Bar = styled('div', {
+  base: {
+    display: 'flex',
+    border: '2px solid',
+    borderColor: 'gray.300',
+    borderRadius: '8px',
+    overflowY: 'hidden',
+    scrollbar: 'hidden',
+  },
+})
+
+const ratingButtonRecipe = cva({
+  base: {
+    backgroundColor: 'white',
+    color: 'initial',
+    border: 'none',
+    borderRadius: 0,
+    padding: '0.5rem 0.85rem',
+    flexGrow: '1',
+    cursor: 'pointer',
+  },
+  variants: {
+    selected: {
+      true: {
+        backgroundColor: '#1d4ed8',
+        color: 'white',
+      },
+    },
+    borderLeft: {
+      true: {
+        borderLeft: '1px solid',
+        borderColor: 'gray.300',
+      },
+    },
+  },
+})
+
+const labelRecipe = cva({
+  base: {
+    color: 'gray.600',
+    paddingTop: '0.25rem',
+  },
+})
+
+export const Rating = ({ maxRating = 8, ...props }) => {
+  const { t } = useTranslation('rooms', { keyPrefix: 'rating' })
+  const [selectedRating, setSelectedRating] = useState<number | null>(null)
+
+  const handleRatingClick = (rating: number) => {
+    setSelectedRating((prevRating) => (prevRating === rating ? null : rating))
+  }
+
+  const minLabel = props?.minLabel ?? t('levels.min')
+  const maxLabel = props?.maxLabel ?? t('levels.max')
+
+  return (
+    <Card>
+      <H lvl={3}>{t('question')}</H>
+      <Bar>
+        {[...Array(maxRating)].map((_, index) => (
+          <Button
+            key={index}
+            onPress={() => handleRatingClick(index + 1)}
+            className={ratingButtonRecipe({
+              selected: selectedRating === index + 1,
+              borderLeft: index != 0,
+            })}
+          >
+            {index + 1}
+          </Button>
+        ))}
+      </Bar>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginBottom: '1rem',
+        }}
+      >
+        <Text variant="sm" className={labelRecipe()}>
+          {minLabel}
+        </Text>
+        <Text variant="sm" className={labelRecipe()}>
+          {maxLabel}
+        </Text>
+      </div>
+      <Button
+        variant="primary"
+        size="sm"
+        fullWidth
+        isDisabled={!selectedRating}
+      >
+        {t('submit')}
+      </Button>
+    </Card>
+  )
+}
