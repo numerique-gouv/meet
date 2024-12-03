b342b9d52610cd99ce12457d45bba3cb8d28da33
lebaudantoine <lebaud.antoine131@gmail.com>
2024-12-03 22:18:55 +0100

    🚩(frontend) enable transcript toggle with a feature flag
    Rely on Posthog for a first iteration on the feature flag feature.
    This is a pragmatic choice, relying on an external dependency might
    not suitable on the longer term, however, compare to the maturity
    of our product, this is the best trade off.

--- src/frontend/src/features/rooms/livekit/components/controls/TranscriptToggle.tsx
+++ src/frontend/src/features/rooms/livekit/components/controls/TranscriptToggle.tsx
@@ -3,12 +3,19 @@ import { RiBardLine } from '@remixicon/react'
 import { useTranslation } from 'react-i18next'
 import { useSidePanel } from '@/features/rooms/livekit/hooks/useSidePanel'
 import { css } from '@/styled-system/css'
+import { useFeatureFlagEnabled } from 'posthog-js/react'
+import { useIsAnalyticsEnabled } from '@/features/analytics/hooks/useIsAnalyticsEnabled'
 
 export const TranscriptToggle = () => {
   const { t } = useTranslation('rooms', { keyPrefix: 'controls.transcript' })
 
   const { isTranscriptOpen, toggleTranscript } = useSidePanel()
   const tooltipLabel = isTranscriptOpen ? 'open' : 'closed'
+
+  const featureEnabled = useFeatureFlagEnabled('transcription-summary')
+  const isAnalyticsEnabled = useIsAnalyticsEnabled()
+
+  if (!featureEnabled && isAnalyticsEnabled) return
 
   return (
     <div
