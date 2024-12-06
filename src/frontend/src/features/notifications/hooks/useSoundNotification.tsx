42bb54b7362ce3763cb024416114c240eed4131a
lebaudantoine <lebaud.antoine131@gmail.com>
2024-12-06 14:55:32 +0100

    ✨(frontend) add notification store for managing user preferences
    Introduced a global state to handle user preferences related to notifications.
    The first use case is sound notifications, allowing users to disable them
    based on feedback.
    
    Additionally, the sound volume is now stored globally, making it easy to
    configure in the future if needed. I've lowered the volume of
    the notifications to make them more discreet.
    
    Preferences are persisted in local storage, ensuring they are retained
    between meetings.

--- src/frontend/src/features/notifications/hooks/useSoundNotification.tsx
+++ src/frontend/src/features/notifications/hooks/useSoundNotification.tsx
@@ -1,7 +1,11 @@
 import useSound from 'use-sound'
+import { useSnapshot } from 'valtio'
+import { notificationsStore } from '@/stores/notifications'
+import { NotificationType } from '@/features/notifications/NotificationType'
 
 // fixme - handle dynamic audio output changes
 export const useNotificationSound = () => {
+  const notificationsSnap = useSnapshot(notificationsStore)
   const [play] = useSound('./sounds/notifications.mp3', {
     sprite: {
       participantJoined: [0, 1150],
@@ -10,9 +14,11 @@ export const useNotificationSound = () => {
       waiting: [2039, 710],
       success: [2740, 1304],
     },
+    volume: notificationsSnap.soundNotificationVolume,
   })
-  const triggerNotificationSound = (type: string) => {
-    play({ id: type })
+  const triggerNotificationSound = (type: NotificationType) => {
+    const isSoundEnabled = notificationsSnap.soundNotifications.get(type)
+    if (isSoundEnabled) play({ id: type })
   }
   return { triggerNotificationSound }
 }
