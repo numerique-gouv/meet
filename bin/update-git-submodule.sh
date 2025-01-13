d8c0c3fa230fd47625d096084418296b8d0fc446
lebaudantoine <lebaud.antoine131@gmail.com>
2025-01-13 17:21:28 +0100

    ♻️(scripts) improve script portability
    Improved script portability by switching to `/usr/bin/env bash`, ensuring
    better support across environments where `bash` may not be the default shell.

--- bin/update-git-submodule.sh
+++ bin/update-git-submodule.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 git submodule update --init --recursive
 git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
