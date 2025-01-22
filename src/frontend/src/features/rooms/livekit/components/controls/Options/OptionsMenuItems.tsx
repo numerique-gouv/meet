1b52d761680311c1b1342ba8458188099238167a
lebaudantoine <lebaud.antoine131@gmail.com>
2025-01-22 14:30:22 +0100

    ♻️(frontend) extract menu items into individual components
    Each menu item is now a standalone component, improving:
    - Code organization & reusability
    - Maintainability by reducing OptionsMenuItems complexity
    
    This breaks down large components.

--- src/frontend/src/features/rooms/livekit/components/controls/Options/OptionsMenuItems.tsx
+++ src/frontend/src/features/rooms/livekit/components/controls/Options/OptionsMenuItems.tsx
@@ -1,28 +1,12 @@
-import {
-  RiAccountBoxLine,
-  RiFullscreenExitLine,
-  RiFullscreenLine,
-  RiMegaphoneLine,
-  RiSettings3Line,
-} from '@remixicon/react'
-import { MenuItem, Menu as RACMenu, MenuSection } from 'react-aria-components'
-import { useTranslation } from 'react-i18next'
+import { Menu as RACMenu, MenuSection } from 'react-aria-components'
 import { Separator } from '@/primitives/Separator'
-import { useSidePanel } from '../../../hooks/useSidePanel'
-import { menuRecipe } from '@/primitives/menuRecipe.ts'
-import { useSettingsDialog } from '../SettingsDialogContext'
-import { GRIST_FORM } from '@/utils/constants'
-import { useFullScreen } from '../../../hooks/useFullScreen'
+import { FullScreenMenuItem } from './FullScreenMenuItem'
+import { SettingsMenuItem } from './SettingsMenuItem'
+import { FeedbackMenuItem } from './FeedbackMenuItem'
+import { EffectsMenuItem } from './EffectsMenuItem'
 
 // @todo try refactoring it to use MenuList component
 export const OptionsMenuItems = () => {
-  const { t } = useTranslation('rooms', { keyPrefix: 'options.items' })
-  const { toggleEffects } = useSidePanel()
-  const { setDialogOpen } = useSettingsDialog()
-
-  const { toggleFullScreen, isCurrentlyFullscreen, isFullscreenAvailable } =
-    useFullScreen()
-
   return (
     <RACMenu
       style={{
@@ -31,49 +15,13 @@ export const OptionsMenuItems = () => {
       }}
     >
       <MenuSection>
-        {isFullscreenAvailable && (
-          <MenuItem
-            onAction={() => toggleFullScreen()}
-            className={menuRecipe({ icon: true, variant: 'dark' }).item}
-          >
-            {isCurrentlyFullscreen ? (
-              <>
-                <RiFullscreenExitLine size={20} />
-                {t('fullscreen.exit')}
-              </>
-            ) : (
-              <>
-                <RiFullscreenLine size={20} />
-                {t('fullscreen.enter')}
-              </>
-            )}
-          </MenuItem>
-        )}
-        <MenuItem
-          onAction={() => toggleEffects()}
-          className={menuRecipe({ icon: true, variant: 'dark' }).item}
-        >
-          <RiAccountBoxLine size={20} />
-          {t('effects')}
-        </MenuItem>
+        <FullScreenMenuItem />
+        <EffectsMenuItem />
       </MenuSection>
       <Separator />
       <MenuSection>
-        <MenuItem
-          href={GRIST_FORM}
-          target="_blank"
-          className={menuRecipe({ icon: true, variant: 'dark' }).item}
-        >
-          <RiMegaphoneLine size={20} />
-          {t('feedback')}
-        </MenuItem>
-        <MenuItem
-          className={menuRecipe({ icon: true, variant: 'dark' }).item}
-          onAction={() => setDialogOpen(true)}
-        >
-          <RiSettings3Line size={20} />
-          {t('settings')}
-        </MenuItem>
+        <FeedbackMenuItem />
+        <SettingsMenuItem />
       </MenuSection>
     </RACMenu>
   )
