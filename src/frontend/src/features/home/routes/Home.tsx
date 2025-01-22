30bde2fe6604343176cd70e7191bda23d0f941d5
lebaudantoine <lebaud.antoine131@gmail.com>
2025-01-22 22:07:26 +0100

    ♻️(frontend) use pandas utilities
    Forgot how panda css is amazing. They provide all kinds of utilities,
    and few for accessibility. Amazing!

--- src/frontend/src/features/home/routes/Home.tsx
+++ src/frontend/src/features/home/routes/Home.tsx
@@ -32,10 +32,10 @@ const Columns = ({ children }: { children?: ReactNode }) => {
         justifyContent: 'normal',
         padding: '0 1rem',
         width: 'calc(100% - 2rem)',
-        '@media(prefers-reduced-motion: reduce)': {
+        _motionReduce: {
           opacity: 1,
         },
-        '@media(prefers-reduced-motion: no-preference)': {
+        _motionSafe: {
           opacity: 0,
           animation: '.5s ease-in fade 0s forwards',
         },
